d029501463b7acf35b83f63edc62638352e368df
bobomarcu <bogdan.ioan.marcu@stud.ubbcluj.ro>
2022-09-30 04:39:15 +0300

    update backend

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express')
-
+const mysql = require('mysql')
 
 const app = express()
+
+app.get('/', (req,res)=>{
+
+    res.send('ok')
+
+})
+
+
+app.listen(4000, ()=>{
+
+    console.log('app runnig')
+
+})
